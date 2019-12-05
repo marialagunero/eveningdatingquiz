00145f8acff6007bff3703503a07d6d4021a5c29
Student <example@example.com>
2019-12-04 19:17:17 -0800

    add all files and modified js and html. Code working

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
-$(document).ready(function(){
-  $(form#dating)
+$(document).ready(function() {
+  $("form#dating").submit(function(event) {
+    event.preventDefault();
+    var colorChoice = parseInt($("#colorChoice").val());
+    var foodChoice = parseInt($("#foodChoice").val());
+    var drinkChoice = parseInt($("#drinkChoice").val());
+    var result = (colorChoice + foodChoice + drinkChoice)
+
+    if (result <= 3) {
+      $("#obama").show();
+      $("#chris, #wonderwoman").hide();
+
+    } else if (result  <= 6) {
+      $("#wonderwoman").show();
+      $("#obama, #chris").hide();
+
+    } else if (result <= 9) {
+      $("#chris").show();
+      $("#wonderwoman, #obama").hide();
 
 
+    return (result)
 
+  };
 
-
-
+  });
 
 });
